1b4b0bf0d1f423ec302af783a8d936666c3190fc
tofilagman <tofilagman@gmail.com>
2025-05-07 21:10:46 +0800

    books

---
+++ src/book.js
@@ -0,0 +1,39 @@
+let books = []
+
+exports.getAll = (req, res) => {
+  res.send(books)
+}
+
+exports.create = (req, res) => {
+  const book = req.body
+  books.push(book)
+  res.send(book)
+}
+
+exports.getById = (req, res) => {
+  const id = parseInt(req.params.id)
+  const book = books.find((t) => t.id === id)
+  if (!book) {
+    res.status(404).send()
+  } else {
+    res.send(book)
+  }
+}
+
+exports.update = (req, res) => {
+  const id = parseInt(req.params.id)
+  const book = books.find((t) => t.id === id)
+  if (!book) {
+    res.status(404).send()
+  } else {
+    const newbook = req.body
+    books = books.map((t) => (t.id === id ? newbook : t))
+    res.send(newbook)
+  }
+}
+
+exports.delete = (req, res) => {
+  const id = parseInt(req.params.id)
+  books = books.filter((t) => t.id !== id)
+  res.status(204).send()
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const swagger = require('./swagger')
 const todo = require('./todo')
+const book = require('./book');
 const http = require("http");
 const https = require("https");
 const fs = require("fs");
@@ -55,17 +56,67 @@ app.put('/todos/:id', todo.update)
 
 /**
  * DELETE /todos/{id}
- * @summary update todo item with specified id
+ * @summary delete todo item with specified id
  * @param {number} id.path - id of todo item
  * @return {object} 200 - success response
  */
 app.delete('/todos/:id', todo.delete)
 
+//---------------------------------------------
+
+/**
+   * BOOK
+   * @typedef {object} Book
+   * @property {number} id.required - Id
+   * @property {string} title.required - Book Title
+   * @property {number} year - Year published
+   */
+
+/**
+ * GET /books
+ * @summary returns all books
+ * @return {array<object>} 200 - success response
+ */
+app.get('/books', book.getAll)
+
+/**
+ * POST /books
+ * @summary create a book
+ * @param {Book} request.body.required - item info
+ * @return {object} 200 - success response
+ */
+app.post('/books', book.create) 
+
+/**
+ * GET /books/{id}
+ * @summary returns all books with specified id
+ * @param {number} id.path - id of book
+ * @return {object} 200 - success response
+ */
+app.get('/books/:id', book.getById)
+
+/**
+ * PUT /books/{id}
+ * @summary update book with specified id
+ * @param {number} id.path - id of book
+ * @param {Book} request.body.required - book info
+ * @return {object} 200 - success response
+ */
+app.put('/books/:id', book.update)
+
+/**
+ * DELETE /books/{id}
+ * @summary delete book with specified id
+ * @param {number} id.path - id of book
+ * @return {object} 200 - success response
+ */
+app.delete('/books/:id', book.delete)
+
 swagger(app);
 
 var httpServer = http.createServer(app);
  
-httpServer.listen(80);
+httpServer.listen(8080);
  
 // var privateKey = fs.readFileSync(__dirname + "/../certs/selfsigned.key", 'utf-8');
 // var cert = fs.readFileSync(__dirname + "/../certs/selfsigned.crt", 'utf-8');
