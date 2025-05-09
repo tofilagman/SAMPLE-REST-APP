4d5bbc76a3a4129b67d2714428b862334c93c34e
lvillareal <lyndonjose.villareal@infor.com>
2025-05-09 11:52:06 +0800

    update 6

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -14,107 +14,57 @@ router.get('/', (req, res) => {
   res.send('Hey! its working!')
 })
 
-// /**
-//    * TODO
-//    * @typedef {object} Todo
-//    * @property {number} id.required - Id
-//    * @property {name} name - Todo Name
-//    * @property {boolean} completed - Status
-//    */
+/**
+   * BOOK
+   * @typedef {object} Book
+   * @property {number} id.required - Id
+   * @property {string} title.required - Book Title
+   * @property {number} year - Year published
+   */
 
-// /**
-//  * GET /todos
-//  * @summary returns all todo list
-//  * @return {array<object>} 200 - success response
-//  */
-// app.get('/todos', todo.getAll)
+/**
+ * GET /books
+ * @summary returns all books
+ * @return {array<object>} 200 - success response
+ */
+router.get('/books', book.getAll)
 
-// /**
-//  * POST /todos
-//  * @summary create a todo item
-//  * @param {Todo} request.body.required - item info
-//  * @return {object} 200 - success response
-//  */
-// app.post('/todos', todo.create) 
+/**
+ * POST /books
+ * @summary create a book
+ * @param {Book} request.body.required - item info
+ * @return {object} 200 - success response
+ */
+router.post('/books', book.create) 
 
-// /**
-//  * GET /todos/{id}
-//  * @summary returns all todo item with specified id
-//  * @param {number} id.path - id of todo item
-//  * @return {object} 200 - success response
-//  */
-// app.get('/todos/:id', todo.getById)
+/**
+ * GET /books/{id}
+ * @summary returns all books with specified id
+ * @param {number} id.path - id of book
+ * @return {object} 200 - success response
+ */
+router.get('/books/:id', book.getById)
 
-// /**
-//  * PUT /todos/{id}
-//  * @summary update todo item with specified id
-//  * @param {number} id.path - id of todo item
-//  * @param {Todo} request.body.required - item info
-//  * @return {object} 200 - success response
-//  */
-// app.put('/todos/:id', todo.update)
+/**
+ * PUT /books/{id}
+ * @summary update book with specified id
+ * @param {number} id.path - id of book
+ * @param {Book} request.body.required - book info
+ * @return {object} 200 - success response
+ */
+router.put('/books/:id', book.update)
 
-// /**
-//  * DELETE /todos/{id}
-//  * @summary delete todo item with specified id
-//  * @param {number} id.path - id of todo item
-//  * @return {object} 200 - success response
-//  */
-// app.delete('/todos/:id', todo.delete)
+/**
+ * DELETE /books/{id}
+ * @summary delete book with specified id
+ * @param {number} id.path - id of book
+ * @return {object} 200 - success response
+ */
+router.delete('/books/:id', book.delete)
 
-// //---------------------------------------------
-
-// /**
-//    * BOOK
-//    * @typedef {object} Book
-//    * @property {number} id.required - Id
-//    * @property {string} title.required - Book Title
-//    * @property {number} year - Year published
-//    */
-
-// /**
-//  * GET /books
-//  * @summary returns all books
-//  * @return {array<object>} 200 - success response
-//  */
-// app.get('/books', book.getAll)
-
-// /**
-//  * POST /books
-//  * @summary create a book
-//  * @param {Book} request.body.required - item info
-//  * @return {object} 200 - success response
-//  */
-// app.post('/books', book.create) 
-
-// /**
-//  * GET /books/{id}
-//  * @summary returns all books with specified id
-//  * @param {number} id.path - id of book
-//  * @return {object} 200 - success response
-//  */
-// app.get('/books/:id', book.getById)
-
-// /**
-//  * PUT /books/{id}
-//  * @summary update book with specified id
-//  * @param {number} id.path - id of book
-//  * @param {Book} request.body.required - book info
-//  * @return {object} 200 - success response
-//  */
-// app.put('/books/:id', book.update)
-
-// /**
-//  * DELETE /books/{id}
-//  * @summary delete book with specified id
-//  * @param {number} id.path - id of book
-//  * @return {object} 200 - success response
-//  */
-// app.delete('/books/:id', book.delete)
-
-//swagger(router);
+swagger(router);
  
  
-app.use("/", router);  
+app.use("/app", router);  
 
 exports.handler = serverless(app);
