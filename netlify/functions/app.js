c6670f3cfb6669144bc35e32a59ad3eef2425f04
lvillareal <lyndonjose.villareal@infor.com>
2025-05-09 11:32:47 +0800

    update

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -8,121 +8,116 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
+const router = express.Router();
+
+router.get('/', (req, res) => {
   res.send('Hey! its working!')
 })
 
-/**
-   * TODO
-   * @typedef {object} Todo
-   * @property {number} id.required - Id
-   * @property {name} name - Todo Name
-   * @property {boolean} completed - Status
-   */
+// /**
+//    * TODO
+//    * @typedef {object} Todo
+//    * @property {number} id.required - Id
+//    * @property {name} name - Todo Name
+//    * @property {boolean} completed - Status
+//    */
 
-/**
- * GET /todos
- * @summary returns all todo list
- * @return {array<object>} 200 - success response
- */
-app.get('/todos', todo.getAll)
+// /**
+//  * GET /todos
+//  * @summary returns all todo list
+//  * @return {array<object>} 200 - success response
+//  */
+// app.get('/todos', todo.getAll)
 
-/**
- * POST /todos
- * @summary create a todo item
- * @param {Todo} request.body.required - item info
- * @return {object} 200 - success response
- */
-app.post('/todos', todo.create) 
+// /**
+//  * POST /todos
+//  * @summary create a todo item
+//  * @param {Todo} request.body.required - item info
+//  * @return {object} 200 - success response
+//  */
+// app.post('/todos', todo.create) 
 
-/**
- * GET /todos/{id}
- * @summary returns all todo item with specified id
- * @param {number} id.path - id of todo item
- * @return {object} 200 - success response
- */
-app.get('/todos/:id', todo.getById)
+// /**
+//  * GET /todos/{id}
+//  * @summary returns all todo item with specified id
+//  * @param {number} id.path - id of todo item
+//  * @return {object} 200 - success response
+//  */
+// app.get('/todos/:id', todo.getById)
 
-/**
- * PUT /todos/{id}
- * @summary update todo item with specified id
- * @param {number} id.path - id of todo item
- * @param {Todo} request.body.required - item info
- * @return {object} 200 - success response
- */
-app.put('/todos/:id', todo.update)
+// /**
+//  * PUT /todos/{id}
+//  * @summary update todo item with specified id
+//  * @param {number} id.path - id of todo item
+//  * @param {Todo} request.body.required - item info
+//  * @return {object} 200 - success response
+//  */
+// app.put('/todos/:id', todo.update)
 
-/**
- * DELETE /todos/{id}
- * @summary delete todo item with specified id
- * @param {number} id.path - id of todo item
- * @return {object} 200 - success response
- */
-app.delete('/todos/:id', todo.delete)
+// /**
+//  * DELETE /todos/{id}
+//  * @summary delete todo item with specified id
+//  * @param {number} id.path - id of todo item
+//  * @return {object} 200 - success response
+//  */
+// app.delete('/todos/:id', todo.delete)
 
-//---------------------------------------------
+// //---------------------------------------------
 
-/**
-   * BOOK
-   * @typedef {object} Book
-   * @property {number} id.required - Id
-   * @property {string} title.required - Book Title
-   * @property {number} year - Year published
-   */
+// /**
+//    * BOOK
+//    * @typedef {object} Book
+//    * @property {number} id.required - Id
+//    * @property {string} title.required - Book Title
+//    * @property {number} year - Year published
+//    */
 
-/**
- * GET /books
- * @summary returns all books
- * @return {array<object>} 200 - success response
- */
-app.get('/books', book.getAll)
+// /**
+//  * GET /books
+//  * @summary returns all books
+//  * @return {array<object>} 200 - success response
+//  */
+// app.get('/books', book.getAll)
 
-/**
- * POST /books
- * @summary create a book
- * @param {Book} request.body.required - item info
- * @return {object} 200 - success response
- */
-app.post('/books', book.create) 
+// /**
+//  * POST /books
+//  * @summary create a book
+//  * @param {Book} request.body.required - item info
+//  * @return {object} 200 - success response
+//  */
+// app.post('/books', book.create) 
 
-/**
- * GET /books/{id}
- * @summary returns all books with specified id
- * @param {number} id.path - id of book
- * @return {object} 200 - success response
- */
-app.get('/books/:id', book.getById)
+// /**
+//  * GET /books/{id}
+//  * @summary returns all books with specified id
+//  * @param {number} id.path - id of book
+//  * @return {object} 200 - success response
+//  */
+// app.get('/books/:id', book.getById)
 
-/**
- * PUT /books/{id}
- * @summary update book with specified id
- * @param {number} id.path - id of book
- * @param {Book} request.body.required - book info
- * @return {object} 200 - success response
- */
-app.put('/books/:id', book.update)
+// /**
+//  * PUT /books/{id}
+//  * @summary update book with specified id
+//  * @param {number} id.path - id of book
+//  * @param {Book} request.body.required - book info
+//  * @return {object} 200 - success response
+//  */
+// app.put('/books/:id', book.update)
 
-/**
- * DELETE /books/{id}
- * @summary delete book with specified id
- * @param {number} id.path - id of book
- * @return {object} 200 - success response
- */
-app.delete('/books/:id', book.delete)
+// /**
+//  * DELETE /books/{id}
+//  * @summary delete book with specified id
+//  * @param {number} id.path - id of book
+//  * @return {object} 200 - success response
+//  */
+// app.delete('/books/:id', book.delete)
 
-swagger(app);
+//swagger(router);
 
 // var httpServer = http.createServer(app);
  
 // httpServer.listen(80);
  
-module.exports = serverless(app);
+app.use("app", router);
 
-// var privateKey = fs.readFileSync(__dirname + "/../certs/selfsigned.key", 'utf-8');
-// var cert = fs.readFileSync(__dirname + "/../certs/selfsigned.crt", 'utf-8');
-// var creds = {
-//   key: privateKey,
-//   cert: cert
-// }
-// var httpsServer = https.createServer(creds, app);
-// httpsServer.listen(443);
+module.exports = serverless(app);
